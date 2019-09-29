34d6a64587ce392913df16b6c72cc5c617d201d8
Leon <leon.ben2892@gmail.com>
2019-09-29 13:14:55 +0300

    Added basic components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank'
 import './App.css';
+import Particles from 'react-particles-js';
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render(){
     return(
       <div className="App">
-        <header className="App-Header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome ro React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*
+       
+        <FaceRecognition />*/}
       </div>
     )
   }
